11407e498cf705f3228bdeb07f251053a8a06624
Przemominecraft <130060928+Przemominecraft@users.noreply.github.com>
2026-01-15 19:55:29 +0100

    Update ustaw-kanal.js

--- commands/ustaw-kanal.js
+++ commands/ustaw-kanal.js
@@ -1,1 +1,30 @@
+// Last update: 15/01/2026
+// Made by .pointer
 
+// Paths
+
+const { SlashCommandBuilder, MessageFlags } = require('discord.js');
+const fs = require('fs').promises;
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ustaw-kanal')
+        .setDescription('Ustaw kanał do przypomnień o bumpach')
+        .setContexts(['Guild'])
+
+        .addChannelOption(opt =>
+            opt.setName('kanal')
+                .setDescription('Wybierz kanal')
+                .setRequired(true)),
+
+    async execute(intr) {
+
+        if (!intr.member.permissions.has('Administrator')) return intr.reply({ content: 'Nie masz uprawnień do użycia tej komendy', flags: MessageFlags.Ephemeral });
+        
+        const channel = intr.options?.getChannel('kanal') ?? null;
+        await fs.writeFile('./kanal.txt', channel.id);
+
+        intr.reply({ content: 'Ustawiono kanał!', flags: MessageFlags.Ephemeral });
+        channel.send('Oto nowy kanał do przypomnień o bumpach');
+    }
+};
